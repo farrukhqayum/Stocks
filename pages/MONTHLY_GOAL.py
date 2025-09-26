75316b2f4d560ff03ec65dc1ed5f80b96fd38b23
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-09-26 13:07:24 +0300

    Update MONTHLY_GOAL.py

--- pages/MONTHLY_GOAL.py
+++ pages/MONTHLY_GOAL.py
@@ -54,7 +54,7 @@ P = st.number_input("Initial Investment ($)", min_value=0.0, value=1000.0, step=
 r = st.number_input("Profit Rate/Trade (%)", min_value=0.0, value=3.75, step=0.01) / 100.0
 months = st.number_input("Number of Months", min_value=1, value=11, step=1)
 max_trades = st.number_input("Maximum Trades per Month", min_value=1, value=7, step=1)
-eff_monthly = (1 + r) ** max_trades - 1
+eff_monthly = ((1 + r) ** max_trades - 1)*100
 
 if st.button("Calculate Investment Growth"):
     actual = [0, 6328, 1302, 2000, 10040, 16000, 15800, -9000, 3000, 3000, 3000, 3000]
