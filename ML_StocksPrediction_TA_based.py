240c680de840d13a09686057c8c0208fc510632a
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-09-26 11:36:29 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -16,6 +16,13 @@ desc = """
         - NEUTRAL: Hold if in the buy times, else stay side-lines, avoid revenge trading/FOMO.
         - STRONG BUYS: Dominate when RSI recovers from bearish zone and is above its SMA (RSI) in yellow and price is above averages.
         - STRONG SELLS: Dominate when RSI is below 42 and falls below.
+- AVOID CHASING
+    - Opportunities are daily, weekly or monthly, don't grab them all.
+    - Chase double bottoms, candles on monthly, or weekly, avoid daily. Decide trend reversals and enter strategically.
+    - Your gut is always wrong, but sometimes you do need gut-feeling.
+    - Risk 5% not more than that as post-covids, there are more day-traders.
+    - Split a decision into three or two buys, and two or three sells.
+    - Better trades are on weekly time-frame, entries could be 4H or 1H decision.
 - USE DIVERGENCE: For market swings (lows, tops) if you plan to trade for 4-6 months hold
 """
 
@@ -1509,6 +1516,7 @@ def run_app():
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
