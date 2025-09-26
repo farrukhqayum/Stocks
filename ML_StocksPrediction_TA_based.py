fd92724d8fc6cd73ea6813f4ba030b8222007a6e
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-09-26 10:11:50 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -1425,15 +1425,14 @@ def tabular_display(df_results):
 def run_app():
 
     st.header("Positional/Swing Trading Guidance")
-    st.markdown(desc)
-    
+    st.markdown(desc)    
     st.header("Just Keep Winning!!!")
-    
     st.markdown(power_of_compounding)
     initial_capital = st.number_input("Initial Capital ($)", min_value=0.0, value=1000.0, step=100.0)
     win_pct = st.number_input("Avg. Win (%)", min_value=0.0, value=3.75, step=0.1) / 100.0
     tax_pct = st.number_input("Tax (%)", min_value=0.0, value=0.0, step=0.1) / 100.0
     num_wins = st.number_input("Number of Trade Wins", min_value=0, value=75, step=1)
+    
     if st.button("Calculate Growth"):
         final_capital = compound_growth(initial_capital, win_pct, num_wins, tax_pct)
         st.write(f"After {num_wins} consecutive wins, your capital grows to: **${final_capital:,.2f}**")
@@ -1473,6 +1472,11 @@ def run_app():
         st.code(row_text)
         dfs, df_results = MakePredictions(TICKERS)
         PlotPredictions(df_results)
+        
+        st.header("Tabular Results")
+        st.text(f"Tablular display of results, Bullish (Green), Bearish (red), Neutral (Gray), stocks touching 3-months high are in 'darkgray'.
+        Use column filters to further fine-tune the stocks to trade/compound & build positions. Good luck!")
+
         tabular_display(df_results)
         for ticker in TICKERS:
             _df = dfs.get(ticker)
@@ -1481,11 +1485,11 @@ def run_app():
                 continue
 
             plot_single_ticker(ticker, _df, df_results)  
-        #del_old_files(path, 14)
         
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
