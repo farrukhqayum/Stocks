c6dda74f6790454f56400c43c6e492c1ee2ea981
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-07 18:48:13 +0300

    Update Traditional_Signals.py

--- pages/Traditional_Signals.py
+++ pages/Traditional_Signals.py
@@ -46,9 +46,6 @@ def add_technical_indicators(df):
     df['Signal_Line'] = df['MACD'].ewm(span=9, adjust=False).mean()
     
     df['SMIIO'], df['SMIIO_Signal'], df['SMIIO_Osc'] = ta.calculate_smiio(df)
-
-    df['Upper_Band'] = df['SMA1'] + (2 * df['Close'].rolling(20).std())
-    df['Lower_Band'] = df['SMA1'] - (2 * df['Close'].rolling(20).std())
     
     df['Volume_MA20'] = df['Volume'].rolling(window=20).mean()
     df['buy_volume'] = (df.Close > df.Close.shift(1)) * df['Volume']
