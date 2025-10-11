cb214b1ebcdd0fc19b3bdfeb3682c83aef97a9d1
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-11 16:22:48 +0300

    Update INDICATORS_EXPLAINED.py

--- pages/INDICATORS_EXPLAINED.py
+++ pages/INDICATORS_EXPLAINED.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Technical Indicators Flashcards", layout="centered")
 
-st.title("Stock Market Technical Indicators Flashcards")
+st.title("Technical Indicators Flashcards")
 
 flashcards = {
     "Simple Moving Average (SMA)": 
