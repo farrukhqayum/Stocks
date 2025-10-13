d61ed24a2c77bc3eec7f6885cd51dce2365059f0
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-13 20:46:40 +0300

    Update 1_COMPOUNDING.py

--- pages/1_COMPOUNDING.py
+++ pages/1_COMPOUNDING.py
@@ -75,7 +75,7 @@ if submitted:
             color=alt.Color('Series', scale=alt.Scale(domain=list(color_map.keys()), range=list(color_map.values())),
                             legend=alt.Legend(title="Legend", orient='top-left'))
         ).properties(
-            title=f'Capital Growth with {num_wins} Wins (Std Dev Bounds)'
+            title=f'Capital Growth with {num_wins} Wins'
         ).interactive()
 
         annotation = alt.Chart(pd.DataFrame({
