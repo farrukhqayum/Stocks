2c873b606550eaa2cf47aa78ae3186045df2128e
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-23 17:03:16 +0300

    Update _ENTRY_POSITION_ANALYZER.py

--- pages/_ENTRY_POSITION_ANALYZER.py
+++ pages/_ENTRY_POSITION_ANALYZER.py
@@ -1102,8 +1102,9 @@ def main():
                         with col2:
                             hit_value = prediction['will_hit']
                             hit_prob = prediction['hit_prob']
+                            rrr = tp_delta/sl_delta
                             st.metric(label="Hits", value=f"{hit_value} ({hit_prob:.1f}%)")
-                            #st.metric("Hit Probability", f"{prediction['hit_prob']:.1f}%")
+                            st.metric("Risk/Reward", f"{rrr:.0f}")
                             st.metric("Confidence", f"{prediction['confidence']:.1f}%")
 
                         if assessment == "Valid":
