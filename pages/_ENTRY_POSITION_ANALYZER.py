380ecf21b36bd0b7c892d50bb8574440bcababd1
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-23 17:24:14 +0300

    Update _ENTRY_POSITION_ANALYZER.py

--- pages/_ENTRY_POSITION_ANALYZER.py
+++ pages/_ENTRY_POSITION_ANALYZER.py
@@ -760,11 +760,12 @@ def plot_analysis(ticker, df, entry_price, timeframe, assessment, prediction=Non
             ax2.axhline(70, color='red', linestyle='--', alpha=0.4, label='Overbought')
             ax2.axhline(30, color='green', linestyle='--', alpha=0.4, label='Oversold')
             ax2.axhline(50, color='gray', linestyle='-', alpha=0.4)
-
-            ax2.scatter(df.index[df['Bull'] == 1], rsi_[df['Bull'] == 1], color='green', marker='^', s=5, alpha=0.3, label='Bull', zorder=7)
-            ax2.scatter(df.index[df['Bear'] == 1], rsi_[df['Bear'] == 1], color='red', marker='v', s=5, alpha=0.3, label='Bear', zorder=8)
-            ax2.scatter(df.index[df['Short'] == 1], rsi_[df['Short'] == 1], color='red', marker='x', s=5, alpha=0.3, label='Short', zorder=10)
-            ax2.scatter(df.index[df['Hold'] == 1], rsi_[df['Hold'] == 1], color='orange', marker='o', s=5, alpha=0.3, label='Hold', zorder=10)
+            
+            _s = 10
+            ax2.scatter(df.index[df['Bull'] == 1], rsi_[df['Bull'] == 1], color='green', marker='^', s=_s, alpha=0.3, label='Bull', zorder=7)
+            ax2.scatter(df.index[df['Bear'] == 1], rsi_[df['Bear'] == 1], color='red', marker='v', s=_s, alpha=0.3, label='Bear', zorder=8)
+            ax2.scatter(df.index[df['Short'] == 1], rsi_[df['Short'] == 1], color='red', marker='x', s=_s, alpha=0.3, label='Short', zorder=10)
+            ax2.scatter(df.index[df['Hold'] == 1], rsi_[df['Hold'] == 1], color='orange', marker='o', s=_s, alpha=0.3, label='Hold', zorder=10)
             ax2.yaxis.set_label_position("right")
             ax2.yaxis.tick_right()
             ax2.set_ylabel('RSI')
