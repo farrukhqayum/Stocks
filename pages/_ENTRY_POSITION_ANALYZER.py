f98cf3eb0c35040e6d3d85146ace6652b0a51c30
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-23 14:31:29 +0300

    Update _ENTRY_POSITION_ANALYZER.py

--- pages/_ENTRY_POSITION_ANALYZER.py
+++ pages/_ENTRY_POSITION_ANALYZER.py
@@ -1098,7 +1098,7 @@ def main():
 
                         st.write(f"**Reasons**: {reasons}")
                         avg_bull, avg_bear = avg_bull_bear_lengths(df)
-                        st.write(f"Average Bull days: {avg_bull:.0f}, Average Bear days: {avg_bear:.0f}")
+                        st.write(f"Average Bull period in {timeframe}: {avg_bull:.0f}, Average Bear in {timeframe}: {avg_bear:.0f}")
                 
                         fig = plot_analysis(ticker, df, entry_price, timeframe, assessment, prediction)
                         st.pyplot(fig)
