6780fd97e9eefe173da6b74aca0d06f110f9e557
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-10-27 22:17:18 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -1145,8 +1145,8 @@ def run_app():
             f'{"Price":>7} | '
             f'{"Take-profit (%)":>18} | '
             f'{"Stop-loss (%)":>18} | '
-            f'{"Will Hit":>8} | '
-            f'{"Probability (%)":<11} | '
+            f'{"Will Hit":>8} '
+            f'{"(%)":<11} | '
             f'{"Signal (TI)":<8} | '
             f'{"Is High":<7}'
         )
@@ -1191,6 +1191,7 @@ def run_app():
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
