9f54a120bee57ac2605eee590a06b65b279037b7
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-11-11 21:37:12 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -716,7 +716,7 @@ def MakePredictions(TICKERS = "AAPL, GOOGL, MSFT"):
             X_train_cls, X_val_cls, y_train_cls, y_val_cls = train_test_split(
                 X_scaled_cls, y_cls, test_size=0.2, random_state=42)
             #model_class = RandomForestClassifier(n_estimators=200, max_depth=10, min_samples_leaf=5, random_state=42)
-            RandomForestClassifier(
+            model_class = RandomForestClassifier(
                 n_estimators=400, 
                 max_depth=12, 
                 min_samples_split=4,
@@ -1203,6 +1203,7 @@ def run_app():
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
