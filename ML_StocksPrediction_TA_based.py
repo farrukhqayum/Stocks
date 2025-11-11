70302dfcbd7802270ae6913eff00b987f6348991
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-11-11 22:08:47 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -712,16 +712,19 @@ def MakePredictions(TICKERS = "AAPL, GOOGL, MSFT"):
             # --- Step 1: Train TP Hit Classifier ---
             X_cls = df_model[FEATURES]
             y_cls = df_model['Hit_Label'].astype(int)
-            scaler_cls = StandardScaler()
-            #X_scaled_cls = scaler_cls.fit_transform(X_cls)
+            
+            X_train_cls, X_val_cls, y_train_cls, y_val_cls = train_test_split(
+                X_scaled_cls, y_cls, test_size=0.2, random_state=42)
+
             pipe = Pipeline([
                 ('scaler', StandardScaler()),
                 ('model', RandomForestClassifier(...))
             ])
+            
             pipe.fit(X_train_cls, y_train_cls)
+            #scaler_cls = StandardScaler()
+            #X_scaled_cls = scaler_cls.fit_transform(X_cls)
 
-            X_train_cls, X_val_cls, y_train_cls, y_val_cls = train_test_split(
-                X_scaled_cls, y_cls, test_size=0.2, random_state=42)
             #model_class = RandomForestClassifier(n_estimators=200, max_depth=10, min_samples_leaf=5, random_state=42)
             model_class = RandomForestClassifier(
                 n_estimators=400, 
@@ -1222,6 +1225,7 @@ def run_app():
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
