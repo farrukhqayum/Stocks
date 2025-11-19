7c6892727858748a6e2e0e019860e2d499aecdd2
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-11-19 20:22:05 +0300

    Update 4_GLOBAL_MONEY_FLOW.py

--- pages/4_GLOBAL_MONEY_FLOW.py
+++ pages/4_GLOBAL_MONEY_FLOW.py
@@ -234,6 +234,10 @@ st.sidebar.markdown("### Pairwise Correlation Heatmap")
 corr_matrix = data.corr()
 
 # Melt correlation matrix for Altair
+print(corr_matrix.reset_index().columns)
+corr_melt = corr_matrix.reset_index().melt('Asset')
+corr_melt.columns = ['Asset1', 'Asset2', 'Correlation']
+
 corr_melt = corr_matrix.reset_index().rename(columns={'index': 'Asset1'}).melt('Asset1')
 corr_melt.columns = ['Asset1', 'Asset2', 'Correlation']
 
