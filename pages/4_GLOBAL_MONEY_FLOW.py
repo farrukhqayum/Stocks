6f1ffed4b569bc5ebce0f40806ead210edad7a68
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-11-19 20:23:49 +0300

    Fix melting of correlation matrix for Altair
    Updated the melting process of the correlation matrix to use 'Ticker' instead of 'index'.

--- pages/4_GLOBAL_MONEY_FLOW.py
+++ pages/4_GLOBAL_MONEY_FLOW.py
@@ -234,9 +234,7 @@ st.sidebar.markdown("### Pairwise Correlation Heatmap")
 corr_matrix = data.corr()
 
 # Melt correlation matrix for Altair
-st.write(corr_matrix.reset_index().columns)
-
-corr_melt = corr_matrix.reset_index().rename(columns={'index': 'Asset1'}).melt('Asset1')
+corr_melt = corr_matrix.reset_index().melt('Ticker')
 corr_melt.columns = ['Asset1', 'Asset2', 'Correlation']
 
 heatmap = (
