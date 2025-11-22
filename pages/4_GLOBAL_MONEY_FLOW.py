815f65dca30b68a7c1a951af12b96056c0a41e14
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-11-22 14:24:06 +0300

    Update 4_GLOBAL_MONEY_FLOW.py

--- pages/4_GLOBAL_MONEY_FLOW.py
+++ pages/4_GLOBAL_MONEY_FLOW.py
@@ -450,5 +450,5 @@ for ticker in ticker_list:
 corr_df = pd.DataFrame(corr_results).dropna()
 corr_df = corr_df.sort_values('Correlation')
 
-st.markdown("### Stock/ETF Correlation with Global Money Flow")
+st.markdown("### Given List's Correlation with Global Money Flow")
 st.dataframe(corr_df, use_container_width=False, height=600, width=700)
