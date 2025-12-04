80ff715961d9eec5a7b81dbadce87096a6548100
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-04 19:03:13 +0300

    Update 2_PAST_PERFORMANCE_ML.py

--- pages/2_PAST_PERFORMANCE_ML.py
+++ pages/2_PAST_PERFORMANCE_ML.py
@@ -603,8 +603,6 @@ if st.button("Run ML Strategy Backtest"):
             current_ml_confidence >= ml_confidence_threshold):
 
             entry_price = float(df_daily.loc[current_date, 'Close'])
-            #TP_price = entry_price * (1 + TP_pct / 100.0)
-            #SL_price = entry_price * (1 - SL_pct / 100.0)
 
             tp_given = TP_pct / 100.0
             sl_given = -SL_pct / 100.0
@@ -614,7 +612,7 @@ if st.button("Run ML Strategy Backtest"):
             
             entry_price = float(df_daily.loc[current_date, 'Close'])
             
-            if tp_given > predicted_return:
+            if tp_given < predicted_return:
                 TP_price = entry_price * (1 + predicted_return)
             else:
                 TP_price = entry_price * (1 + tp_given)
