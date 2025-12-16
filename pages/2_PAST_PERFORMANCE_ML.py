a4efaaffdd098b5524a32dda0b5229cac09153cd
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-16 13:42:12 +0100

    Update 2_PAST_PERFORMANCE_ML.py

--- pages/2_PAST_PERFORMANCE_ML.py
+++ pages/2_PAST_PERFORMANCE_ML.py
@@ -171,16 +171,16 @@ with col1:
 with col2:
     period = st.selectbox("History period", ["1y", "2y", "3y", "5y", "7y"], index=2)
 with col3:
-    TP_pct = st.number_input("TP (%)", value=3.75, step=0.5, help="3-10% are logical to add more training data. 15% are less likely and not repeatable.")
+    TP_pct = st.number_input("TP (%)", value=3.75, min_value=0, max_value=100, step=0.5, help="3-10% are logical to add more training data. 15% are less likely and not repeatable.")
 with col4:
-    SL_pct = st.number_input("SL (%)", value=7.0, step=0.5, help="To hit less 'SL', try to use 2x TP e.g. 14% as a number")
+    SL_pct = st.number_input("SL (%)", value=7.0, min_value=0, max_value=100, step=0.5, help="To hit less 'SL', try to use 2x TP e.g. 14% as a number")
 
 # ML prediction settings
 col5, col6 = st.columns(2)
 with col5:
     ml_confidence_threshold = st.number_input("ML Confidence Threshold", min_value=0, max_value=100,  value=63, step=5, help = "30% Strongly Bearish, 70% Strongly Bullish")
 with col6:
-    max_holding_days = st.number_input("Max Holding Days", min_value=3, max_value=60, value=15, step=2, help = "This is like a forced stop-loss, make it 15-21 days.")
+    max_holding_days = st.number_input("Max Holding Days", min_value= 1, max_value=91, value=15, step= 1, help = "This is like a forced stop-loss, make it 15-21 days.")
 
 # -------------------------
 # Technical Analysis Functions (Simplified)
