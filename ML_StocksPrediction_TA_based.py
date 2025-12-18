c7e2ebd258c0c930b924b813479961617ba8aead
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-18 15:03:34 +0100

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -19,7 +19,7 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 st.caption("Data sourced via Yahoo Finance • Updated dynamically")
 warnings.filterwarnings("ignore")
-st.set_page_config(layout="wide", page_title="MAIN - Machine Learning of Stocks")
+st.set_page_config(layout="wide", page_title="📈 MAIN - Machine Learning of Stocks")
 
 bold = '\033[1m'
 end = '\033[0m'
@@ -1435,6 +1435,7 @@ def run_app():
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
