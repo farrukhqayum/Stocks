c5b5a5b745596977a01c658f3b35485bb0f50a49
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-19 15:50:20 +0100

    Update 2_PAST_PERFORMANCE_ML.py

--- pages/2_PAST_PERFORMANCE_ML.py
+++ pages/2_PAST_PERFORMANCE_ML.py
@@ -120,7 +120,7 @@ with st.expander("🤖 Example Backtest Results"):
     
     st.image("pages/ML_backtest_COIN.png",
              caption="COIN ML Backtest Results",
-             width=850,
+             width=500,
              use_column_width=False)
 
 # -------------------------
