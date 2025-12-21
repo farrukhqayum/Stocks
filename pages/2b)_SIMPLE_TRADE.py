9e262eeeefeeeab05d74b5a3994f0b862dbd9970
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-21 23:09:53 +0100

    Update 2b)_SIMPLE_TRADE.py

--- pages/2b)_SIMPLE_TRADE.py
+++ pages/2b)_SIMPLE_TRADE.py
@@ -224,7 +224,7 @@ if len(trades) > 0:
         if len(winners) > 0:
             ax1.scatter(winners['exit_date'], winners['exit_price'], color='green', marker='o', s=30, label = f'Winners ({len(winners)})', zorder=9, edgecolors='darkgreen', linewidths=0.7)
         if len(losers) > 0:
-            ax1.scatter(losers['exit_date'], losers['exit_price'], color='red', marker='o', s=30,  label = f'Losses ({len(losses)})', zorder=10, edgecolors='black', linewidths=0.7)
+            ax1.scatter(losers['exit_date'], losers['exit_price'], color='red', marker='o', s=30,  label = f'Losses ({len(losers)})', zorder=10, edgecolors='black', linewidths=0.7)
 
 ax1.set_title(f'{ticker} - Simple RSI+SMA | Return: {total_return:+.1f}% | {len(trades)} Trades', 
               fontsize=16, fontweight='bold', pad=15)
