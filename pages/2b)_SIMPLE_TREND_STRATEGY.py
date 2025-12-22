c107bc42ac276208ae4ef59e60447f514351f8b1
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-22 14:19:26 +0100

    Update 2b)_SIMPLE_TREND_STRATEGY.py

--- pages/2b)_SIMPLE_TREND_STRATEGY.py
+++ pages/2b)_SIMPLE_TREND_STRATEGY.py
@@ -300,7 +300,6 @@ for i in range(len(df_bt)):
 for i in range(len(x)-1):
     ax1.plot(x[i:i+2], y[i:i+2], color=colors[i], linewidth=2, alpha=0.5)
 
-#ax1.plot(df_bt.index, df_bt['Close'], c='gray', linewidth=2, label='Close', alpha=0.5)
 ax1.plot(df_bt.index, df_bt['SMA_FAST'], color='orange', linewidth=2, label=f'SMA{sma_fast_len}', alpha=0.5)
 ax1.plot(df_bt.index, df_bt['SMA_SLOW'], color='red', linewidth=2, label=f'SMA{sma_slow_len}', alpha=0.5)
 
@@ -339,12 +338,8 @@ ax1.set_ylabel('Price ($)', fontsize=12, fontweight='bold')
 ax1.legend(loc='upper left', fontsize=8, ncol=2)
 ax1.grid(True, alpha=0.2, linestyle='--')
 ax1.set_facecolor('#F8F9FA')
-ax1 = axes[0]
-ax1_right = ax1.twinx()
-ax1_right.plot(df_bt.index, df_bt['Close'].pct_change(20)*100, 
-               color='orange', linewidth=1, label='20d %Chg')
-ax1_right.set_ylabel('20d % Change', color='orange')
-ax1_right.tick_params(axis='y', colors='orange')
+ax1.yaxis.tick_right()     
+ax1.yaxis.set_label_position("right")
 
 ax2 = axes[1]
 ax2.plot(df_bt.index, df_bt['RSI'], color='#9B59B6', linewidth=1.1, label=f'RSI({rsi_len})', alpha = 0.7)
@@ -357,11 +352,8 @@ ax2.legend(loc='upper left', fontsize=9)
 ax2.grid(True, alpha=0.2)
 ax2.set_ylim(0, 100)
 ax2.set_facecolor('#F8F9FA')
-ax2_right = ax2.twinx()
-ax2_right.plot(df_bt.index, df_bt['RSI_EMA_ROC']*100, 
-               color='purple', linewidth=1, label='RSI Momentum')
-ax2_right.set_ylabel('RSI Momentum %', color='purple')
-ax2_right.tick_params(axis='y', colors='purple')
+ax2.yaxis.tick_right()       
+ax2.yaxis.set_label_position("right")
 
 ax3 = axes[2]
 ax3.plot(df_bt.index, df_bt['DI+'], color='green', linewidth=1, label='DI+', alpha=0.5)
@@ -372,11 +364,8 @@ ax3.set_ylabel('ADX/DI', fontsize=11, fontweight='bold')
 ax3.legend(loc='upper left', fontsize=9)
 ax3.grid(True, alpha=0.2)
 ax3.set_facecolor('#F8F9FA')
-ax3_right = ax3.twinx()
-ax3_right.plot(df_bt.index, df_bt['ADX_ROC']*100, 
-               color='gray', linewidth=1, label='ADX Momentum')
-ax3_right.set_ylabel('ADX ROC %', color='gray')
-ax3_right.tick_params(axis='y', colors='gray')
+ax3.yaxis.tick_right()       
+ax3.yaxis.set_label_position("right")
 
 
 ax4 = axes[3]
@@ -389,6 +378,8 @@ ax4.set_xlabel('Date', fontsize=12, fontweight='bold')
 ax4.legend(loc='upper left', fontsize=9)
 ax4.grid(True, alpha=0.2)
 ax4.set_facecolor('#F8F9FA')
+ax4.yaxis.tick_right()    
+ax4.yaxis.set_label_position("right")
 
 plt.tight_layout()
 st.pyplot(fig)
