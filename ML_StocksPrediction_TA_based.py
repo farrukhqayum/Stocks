854372810ce84d801122912d6678739871dd3614
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-22 15:59:10 +0100

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -253,8 +253,8 @@ def add_technical_indicators(df):
                 (df['RSI'].between(50, 90)) &
                 ((df['ADX'] > 24) & (df['+DI'] > df['-DI']))
             )
-        ]
-    ]
+            ]
+
 
     choices = ['Bull', 'Bear', 'Short', 'Hold']
     df['TI'] = np.select(conditions, choices, default='Neutral')
@@ -1457,6 +1457,7 @@ def run_app():
 # Call this only in streamlit run mode
 if __name__ == "__main__":
     run_app()
+
 
 
 
