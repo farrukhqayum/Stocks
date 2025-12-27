46530ba95dc8c892bb0e78ea45417d15d433d802
Farrukh Qayyum (P) <farrukhqayum@gmail.com>
2025-12-27 14:24:01 +0100

    Update 2a_PAST_PERFORMANCE_ML.py

--- pages/2a_PAST_PERFORMANCE_ML.py
+++ pages/2a_PAST_PERFORMANCE_ML.py
@@ -1041,7 +1041,7 @@ if st.button("Run ML Strategy Backtest"):
             if (
                 current_ml_signal in ['TP', 'Hold', 'None']
                 and current_ml_confidence >= ml_confidence_threshold
-                and (ema1 * 0.97 <= close <= ema1 * 1.02)):
+                and (ema1 * 0.88 <= close <= ema1 * 1.16)):
     
                 entry_price = float(current_data.loc[current_date, 'Close'])
     
