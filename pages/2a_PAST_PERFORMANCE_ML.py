32878b6432f63be575b216771b06ec3517cef0e8
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-01-11 13:58:25 +0300

    Update 2a_PAST_PERFORMANCE_ML.py

--- pages/2a_PAST_PERFORMANCE_ML.py
+++ pages/2a_PAST_PERFORMANCE_ML.py
@@ -998,15 +998,12 @@ if st.button("Run ML Strategy Backtest"):
 
         if not in_trade:
             current_data = df_daily.iloc[:i+1]
-            
+           
             if len(current_data) < 100:
                 continue
-            
-            training_data = current_data.iloc[:current_data]           
 
             if i % RETRAIN_EVERY == 0 or i < 120:
                 models = train_ml_models(current_data)
-                models = train_ml_models(training_data)
     
             if models[0] is None:
                 continue
