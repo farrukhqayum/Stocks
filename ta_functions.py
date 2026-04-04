88ae18345801d44140bd42c1b8b9dff41ee1e917
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-04-04 13:40:12 +0300

    Update ta_functions.py

--- ta_functions.py
+++ ta_functions.py
@@ -607,6 +607,7 @@ def add_candlestickpatterns(df):
 
     return df
 
+
 def detect_divergences(df, period=14, max_bar_diff=3):
     """
     Detect regular and hidden divergences:
@@ -737,5 +738,4 @@ def plot_divergences(df, bullish, bearish, hidden_bull, hidden_bear, double_tops
     # Double bottoms (RSI) with purple dotted lines on RSI only (optional)
     for i1, i2 in double_bottoms:
         ax_rsi.plot([df.index[i1], df.index[i2]], [df['RSI'].iloc[i1], df['RSI'].iloc[i2]], color='purple', alpha=alpha_val, lw=1.2, linestyle='dotted')
-
 
