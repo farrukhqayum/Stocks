0ad6f8b9232e1ee2d730e8cb15804dca864ab592
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-04-04 13:59:29 +0300

    Update 1_ENTRY_POSITION_ANALYZER.py

--- pages/1_ENTRY_POSITION_ANALYZER.py
+++ pages/1_ENTRY_POSITION_ANALYZER.py
@@ -195,8 +195,6 @@ def add_technical_indicators(df, timeframe='1D'):
         df['ATR'] = ta.calculate_atr(high=df.High, low=df.Low, close=df.Close)
         df = ta.scaled_volatility(df)
         df = ta.add_candlestickpatterns(df)
-        st.write("DEBUG candlestick return type:", type(df))
-        st.write("DEBUG candlestick columns:", getattr(df, "columns", None))
         df['RSI']= ta.calculate_rsi(df)
         df['RSI_SMA'] = df['RSI'].rolling(14).mean()
         
