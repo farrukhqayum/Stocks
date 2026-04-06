1f2ffbb402f58d2cf8aae01584c1510cfa137920
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-04-06 20:40:23 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -9,7 +9,7 @@ import time
 from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
-import yfinance as yf
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import altair as alt
