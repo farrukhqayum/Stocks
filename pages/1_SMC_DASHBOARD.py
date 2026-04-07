4a2916ff01fbfa9e0a5571ca3f29c8aa856e23c6
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-04-07 20:40:50 +0300

    Update 1_SMC_DASHBOARD.py

--- pages/1_SMC_DASHBOARD.py
+++ pages/1_SMC_DASHBOARD.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="SMC Dashboard", layout="wide")
 st.title("📈 SMART MONEY CONCEPTS (SMC)")
 
+from imports import *
 # ---------------------------------------------------------
 # INDICATORS
 # ---------------------------------------------------------
