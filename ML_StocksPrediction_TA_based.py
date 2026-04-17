8eb670c4724a30918a80ec8c7d1125ec07b5a5a2
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-04-17 15:23:48 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -1,13 +1,18 @@
 import streamlit as st
-st.cache_data.clear()
-st.cache_resource.clear()
+
+st.set_page_config(layout="wide", page_title="📈 MAIN - Machine Learning of Stocks")
+
+try:
+    st.cache_data.clear()
+    st.cache_resource.clear()
+except:
+    pass
 st.empty()
 if 'session_initialized' not in st.session_state:
     st.session_state.session_initialized = True
     st.session_state.ml_results = None
     st.session_state.cache_cleared = True
     
-st.set_page_config(layout="wide", page_title="📈 MAIN - Machine Learning of Stocks")
 from imports import *
 
 st.caption("Data sourced via Yahoo Finance • Updated dynamically")
