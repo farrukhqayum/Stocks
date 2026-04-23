81200edee63093c91c27059dc041ded0a0345010
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-04-24 00:16:34 +0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from time import sleep
 from pathlib import Path
 import warnings
-import alpha_vantage_loader as av
+import requests
 
 # Third‑Party Libraries
 import pandas as pd
