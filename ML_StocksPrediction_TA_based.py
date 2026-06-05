41a81f8bc26e865884e051834eb99460b7e11cb7
Farrukh Qayyum <farrukhqayum@gmail.com>
2026-06-05 09:43:35 +0300

    Update ML_StocksPrediction_TA_based.py

--- ML_StocksPrediction_TA_based.py
+++ ML_StocksPrediction_TA_based.py
@@ -183,7 +183,7 @@ FEATURES = [
     'Candlesticks', 'gapStrength',
 
     # Market Sentiment & Signals
-    'Bear', 'Bull', 'Short', 'Hold', 'Neutral', 'StrongBull', 'StrongBear', 'Neutral', 'Exhaustion',
+    'Bear', 'Bull', 'Short', 'Hold', 'Neutral', 'StrongBull', 'StrongBear', 'Exhaustion',
 
     # PIVOTS
     'PP_Avg', 'R1_Avg', 'R2_Avg', 'S1_Avg', 'S2_Avg'
